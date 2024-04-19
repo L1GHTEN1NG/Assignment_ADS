9f5e202308e1be4477fcf2dcbe731ca3f2e3f35c
L1GHTEN1NG <230670@astanait.edu.kz>
2024-04-19 08:41:19 +0500

    Spidy

--- src/Defense/src/Main.java
+++ src/Defense/src/Main.java
@@ -7,26 +7,21 @@ public class Main {
         System.out.print("Give me value of string: ");
         String s = scanner.nextLine();
 
-        System.out.print("Give me character: ");
-        char a = 'a';
+        System.out.print("Give me symbol: ");
+        char symbol = scanner.next().charAt(0);
 
-
-        System.out.print("Number of character " + a + " in string " + s + " is " + CheckCharacter(a, s));
+        System.out.print("Number of character " + symbol + " in string " + s + " is " + CheckCharacter(symbol, s, 0));
     }
 
-    public static int CheckCharacter(char a, String s){
-        int value = 0;
-        for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) == s.charAt(0)){
-                break;
-            }
-
-            if (s.charAt(i) == a){
-                value++;
-                CheckCharacter(a, s);
-            }
+    public static int CheckCharacter(char c, String s, int index){
+        if (index >= s.length()){
+            return 0;
         }
 
-        return value;
+        if (s.charAt(index) == c){
+            return 1 + CheckCharacter(c, s, index + 1);
+        } else{
+            return CheckCharacter(c, s, index + 1);
+        }
     }
 }
